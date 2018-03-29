faaea771f548f1e71433e1638924043c452a6439
Mr.Wei <1453925044@qq.com>
2018-03-29 14:41:30 +0800

    修改

--- public/js/index.js
+++ public/js/index.js
@@ -1,27 +1,26 @@
-$(function(){
-    var nowimg=0;
-    var timer=null;
-    $(".head-banner img:first").clone().appendTo('.head-banner')
-    function rightFunc(){
-        if(nowimg<2){
+$(function () {
+    var nowimg = 0;
+    var timer = null;
+    $(".head-banner li:first").clone().appendTo('.head-banner').css({
+        "background-image": "url('./images/home-5038.jpg')", "background-size": "100% 100%",
+        "background-repeat": "no-repeat"
+    })
+
+    function rightFunc() {
+        if (nowimg < 2) {
             nowimg++
-            $(".head-banner").animate({"left":nowimg*-1347},1000)
+            $(".head-banner").animate({ "left": nowimg * -1347 }, 1500)
         }
-        else{
-            nowimg=0
-            $(".head-banner").animate({"left":3*-1347},1000,function(){
-                $(".head-banner").css("left",0)
+        else {
+            nowimg = 0
+            $(".head-banner").animate({ "left": 3 * -1347 }, 1000, function () {
+                $(".head-banner").css("left", 0)
 
             })
         }
     }
-    timer=setInterval(rightFunc,2000)
-
-    $(".head-banner").mouseenter(function(){
+    timer = setInterval(rightFunc, 2500)
+    $(".head-banner").mouseenter(function () {
         clearInterval(timer)
-    })
-    $(".head-banner").mouseout(function(){
-        clearInterval(timer)
-        timer=setInterval(rightFunc,2000)
     })
 })
