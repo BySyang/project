03492108e16394c11353b62a4fd7b390e4e972f3
Mr.Wei <1453925044@qq.com>
2018-03-30 15:38:24 +0800

    第二次更新

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,11 @@
-$(function () {
+$(function() {
 
     // 轮播js
     var nowimg = 0;
     var timer = null;
     $(".head-banner li:first").clone().appendTo('.head-banner').css({
-        "background-image": "url('./images/首页banner/home-5038.jpg')", "background-size": "100% 100%",
+        "background-image": "url('./images/首页banner/home-5038.jpg')",
+        "background-size": "100% 100%",
         "background-repeat": "no-repeat"
     })
 
@@ -12,22 +13,21 @@ $(function () {
         if (nowimg < 2) {
             nowimg++
             $(".head-banner").animate({ "left": nowimg * -1347 }, 1500)
-        }
-        else {
+        } else {
             nowimg = 0
-            $(".head-banner").animate({ "left": 3 * -1347 }, 2000, function () {
+            $(".head-banner").animate({ "left": 3 * -1347 }, 2000, function() {
                 $(".head-banner").css("left", 0)
 
             })
         }
     }
     timer = setInterval(rightFunc, 4000)
-    $(".head-banner").mouseenter(function () {
+    $(".head-banner").mouseenter(function() {
         clearInterval(timer)
     })
 
     // 导航js
-    $(window).scroll(function () {
+    $(window).scroll(function() {
         var scrollTop = $(this).scrollTop();
         if (scrollTop > 10) {
             $(".head-nav").addClass("head-nav1")
@@ -41,13 +41,3 @@ $(function () {
 
 
 })
-
-
-
-
-
-
-
-
-
-
