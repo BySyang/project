0e55bc5a149aadab9a56024245efe9459b1f6765
Mr.Wei <1453925044@qq.com>
2018-04-01 17:05:38 +0800

    index.css更改

--- public/js/index.js
+++ public/js/index.js
@@ -45,5 +45,20 @@ $(window).scroll(function() {
     }
 })
 
+// 登录注册js  
+    $('.icon-gerenzhongxin').on('click', function(){
+        layer.open({
+            type: 2,
+            title: false,
+            shade: [0.8],
+            maxmin: false,
+            shadeClose: true, 
+            // anim: 2,
+            content: ['login.html', 'no'], //iframe的url，no代表不显示滚动条
+            area : ['680px' , '458px'],
+            content: 'login.html'
+          });
+      });
 
-})
+      
+    })
