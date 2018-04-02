5cfb7abbc6efc7dbbfb21a0e630f0e4b3ea7bd69
Redbreans <1064593216@qq.com>
2018-04-02 17:41:48 +0800

    系列-更改路径
    Signed-off-by: Redbreans <1064593216@qq.com>

--- public/js/Product_series.js
+++ public/js/Product_series.js
@@ -1,3 +1,50 @@
 /**
  * Created by Admin on 2018/3/30.
  */
+function margin() {
+    $(".firstList dl:nth-child(1),.firstList dl:nth-child(5)").css({"margin-left": "0px"});
+    $(".firstList dl:nth-child(5),.firstList dl:nth-child(6),.firstList dl:nth-child(7),.firstList dl:nth-child(8)").css({"margin-top": "17px"});
+}
+margin();
+//    添加图片
+var imgArray = [
+    "fashion_img/series/sports/sports_goods/sports_goods1/sportsGoods1_mid.jpg",
+    "fashion_img/series/sports/sports_goods/sports_goods2/sportsGoods2_mid.jpg",
+    "fashion_img/series/sports/sports_goods/sports_goods3/sportsGoods3_mid.jpg",
+    "fashion_img/series/sports/sports_goods/sports_goods4/sportsGoods4_mid.jpg",
+    "fashion_img/series/sports/sports_goods/sports_goods5/sportsGoods5_mid.jpg",
+    "fashion_img/series/sports/sports_goods/sports_goods6/sportsGoods6_mid.jpg",
+    "fashion_img/series/sports/sports_goods/sports_goods7/sportsGoods7_mid.jpg",
+    "fashion_img/series/sports/sports_goods/sports_goods8/sportsGoods8_mid.jpg",
+    "fashion_img/series/sports/sports_goods/sports_goods9/sportsGoods9_mid.jpg",
+    "fashion_img/series/sports/sports_goods/sports_goods10/sportsGoods10_mid.jpg",
+    "fashion_img/series/sports/sports_goods/sports_goods11/sportsGoods11_mid.jpg",
+    "fashion_img/series/sports/sports_goods/sports_goods12/sportsGoods12_mid.jpg",
+    "fashion_img/series/sports/sports_goods/sports_goods13/sportsGoods13_mid.jpg",
+    "fashion_img/series/sports/sports_goods/sports_goods14/sportsGoods14_mid.jpg",
+    "fashion_img/series/sports/sports_goods/sports_goods15/sportsGoods15_mid.jpg",
+    "fashion_img/series/sports/sports_goods/sports_goods16/sportsGoods16_mid.jpg",
+    "fashion_img/series/sports/sports_goods/sports_goods17/sportsGoods17_mid.jpg",
+    "fashion_img/series/sports/sports_goods/sports_goods18/sportsGoods18_mid.jpg",
+    "fashion_img/series/sports/sports_goods/sports_goods19/sportsGoods19_mid.jpg",
+    "fashion_img/series/sports/sports_goods/sports_goods20/sportsGoods20_mid.jpg",];
+var index = 0;
+$(".smimg").each(function (index, ele) {
+    $(this).html("<img src='" + imgArray[index] + "'/>");
+});
+$(".smimg img").css({"width": "100%"});
+
+function xingMargin() {
+    //    星级上边距
+    $(".bigdl dt:nth-child(2)").css({"margin-top": "6px"});
+    //商品名
+    $(".bigdl dt:nth-child(3)").css({
+        "color": "#434343",
+        "margin-top": "6px",
+        "font-size": "15px",
+        "margin-left": "11px",
+        "font-weight": "normal"
+    });
+    $(".bigdl dt:nth-child(4)").css({"margin-left": "11px", "margin-top": "6px"});
+}
+xingMargin();
