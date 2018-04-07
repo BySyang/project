2c2ef8c48f7aaa683f95808b4bdea10c28fc7b26
BySyang <30365359+BySyang@users.noreply.github.com>
2018-04-07 16:31:00 +0800

    .

--- modal/userAddrModal.js
+++ modal/userAddrModal.js
@@ -3,7 +3,6 @@ const sqlPool = require('./sqlPool');
 let sql = 'select * from users_address ';
 module.exports = {
     userAddr(userId, fn) {
-        console.log(userId)
         sqlPool(sql + ' where userId=?', [userId], fn);
     }
 }
