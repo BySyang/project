ddf62ca458983552bafdb5c2fd9020cbb491046d
Mr.Wei <1453925044@qq.com>
2018-04-08 14:22:47 +0800

    首页小修改

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,4 @@
+
 $(function () {
     // 轮播js
     var nowimg = 0;
@@ -181,10 +182,7 @@ $('#logBtn').click(function () {
         data: data,
         success(data) {
             if (data =='ok') {
-                window.parent.location.reload(function(){
-                    $(".out").css({"display":"block"})
-                    console.log("11111")
-                });
+                window.parent.location.reload()
             }
              else if(username.length==0 && password.length>0) {   
                 layer.msg('账号不能为空，请输入账号');
@@ -262,7 +260,8 @@ $('#regBtn').click(function () {
         success(data) {
             if (data == 'ok') {
                 console.log('注册成功');
-            } else {
+            } 
+            else {
                 consoel.log('注册失败');
             }
         },
