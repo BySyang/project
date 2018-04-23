e7b77d74aaa982a369c77f6aac2fb4eb7c07a286
BySyang <30365359+BySyang@users.noreply.github.com>
2018-04-23 09:58:25 +0800

    修改端口

--- modal/sqlPool.js
+++ modal/sqlPool.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 module.exports = (function () {
     var pool = mysql.createPool({
         connectionLimit: 2,
-        host: '172.16.13.12',
+        host: '172.16.13.7',
         user: 'root',
         password: 'root',
         database: 'project'
