ae027d170a88759a2cf7383a8ee824de8fe7f1fe
summerKun <1344846174@qq.com>
2018-04-28 10:47:38 +0800

    up

--- modal/orderModal.js
+++ modal/orderModal.js
@@ -1,6 +1,6 @@
 /* 订单 */
 const sqlPool = require('./sqlPool');
-let sql =  `SELECT * FROM orders a, order_goods b,goods c , goods_specs d WHERE a.userId=? AND a.orderId=b.orderId AND b.goodsId=c.goodsId=d.goodsId`;
+let sql =  `SELECT * FROM orders a, order_goods b,goods c , goods_specs d,pays e WHERE a.userId=? AND a.orderId=b.orderId AND b.goodsId=c.goodsId=d.goodsId AND a.orderPayMethod=e.id`;
 module.exports = {
     orderList(userId,fn){
         sqlPool(sql,[userId],fn);
