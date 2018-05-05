7d4ef11315c6bb242a1a1cef2c7daa459a6aa1cf
BySyang <30365359+BySyang@users.noreply.github.com>
2018-05-05 18:45:07 +0800

    999

--- modal/catModal.js
+++ modal/catModal.js
@@ -6,4 +6,9 @@ module.exports = {
     catList(userId,fn){
         sqlPool(sql,[userId],fn);
     },
+    catAdd(arr,fn){
+        let sql = 'insert into cats values(null,?,?,?,?,?,0,default)';
+        //用户id,商品id,商品价格,商品数量,商品规格,是否取消,时间.
+        sqlPool(sql,arr,fn)
+    }
 }
