e5a0c2f88d9f85a319f869b6a2433100350f2c75
BySyang <30365359+BySyang@users.noreply.github.com>
2018-05-05 21:39:07 +0800

    完善购物车

--- static/js/shopping.js
+++ static/js/shopping.js
@@ -4,27 +4,64 @@ $(function () {
   let number_val = parseInt($('.number_show').val());
   let danjia = parseInt($('.shopping_price').text());
   let xiaojie = parseInt($('.shopping_subtotal').text());
-  console.log(danjia)
-  $('.number_redu').on('click', function () {
-    if (number_val > 1) {
-      number_val -= 1;
-      $('.number_show').val(number_val);
-      xiaojie=danjia*number_val;
-      $('.shopping_subtotal').text(xiaojie+'元');
-    }
-  });
-  $('.number_add').on('click', function () {
-    if (number_val < 99) {
-      number_val += 1;
-      $('.number_show').val(number_val);
-      xiaojie=danjia*number_val;
-      $('.shopping_subtotal').text(xiaojie+'元');
-    }
-  });
-  $('.shop_check').click(function(){
-    $(this).toggleClass('shop_checked');
-  })
-  
 
-  $('')
+  //全选
+  $('#allcheck').click(function () {
+    $(this).toggleClass('shop_checked');
+    if ($(this).hasClass('shop_checked')) {
+      $('.check').addClass('shop_checked');
+    } else {
+      $('.check').removeClass('shop_checked');
+    }
+    catCalc();
+
+  })
+  //单选
+  $(document).on('click', '.check', function () {
+    $(this).toggleClass('shop_checked');
+    catCalc();
+  })
+  //商品减
+  $(document).on('click', '.number_redu', function () {
+    var nowNum = $(this).siblings('input').val();
+    var newNum = $(this).siblings('input').val(nowNum > 1 ? --nowNum : nowNum).val();
+    $(this).parents('li').siblings('.shopping_subtotal').text($(this).parents('li').siblings('.shopping_price').data('price') * newNum+'元')
+    if (nowNum != newNum) catCalc();
+  })
+  //商品加
+  $(document).on('click', '.number_add', function () {
+    var nowNum = $(this).siblings('input').val();
+    var newNum = $(this).siblings('input').val(++nowNum).val();
+    console.log(newNum)
+    $(this).parents('li').siblings('.shopping_subtotal').text($(this).parents('li').siblings('.shopping_price').data('price') * newNum+'元')
+    catCalc();
+  })
+  //购物车算法
+  function catCalc() {
+    var allNUm = 0;
+    var allMoney = 0;
+    var checkNum = 0;
+    $('.check').each(function () {
+      if ($(this).hasClass('shop_checked')) {
+        var price = parseInt($(this).parents('ul').find('.shopping_price').data('price'));
+        var num = parseInt($(this).parents('ul').find('.shopping_number input').val());
+        allMoney += price * num;
+        allNUm += num;
+        checkNum++;
+      }
+    });
+    $('#shopping_allnum').text(checkNum);
+    $('#shopping_checknum').text(allNUm);
+    $('#shopping_subtotal').text(allMoney);
+  }
+  //购物车结算
+  $('#shopping_jiesuan').click(() => {
+    var ul = $($('.shopping_sec').find('ul').has('.shop_checked'));
+    var goodsId = ul.data('id');
+    var price = ul.find('.shopping_price').data('price');
+    var num = ul.find('input').val();
+    var spec = ul.data('spec');
+    console.log([goodsId, price, num, spec])
+    
+  })
 });
